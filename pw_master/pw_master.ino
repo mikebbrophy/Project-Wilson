0708d1e16a3fd6a9bca9af6c8fb7a8ce43f66837
mikebbrophy <63438634+mikebbrophy@users.noreply.github.com>
2020-04-10 17:44:56 -0700

    Add files via upload

--- pw_master/pw_master.ino
+++ pw_master/pw_master.ino
@@ -1,38 +1,58 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <SHT1x.h>
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
 
 //Globals
 //***** PHOTOCELL *****
 int sensorPin = A1;    // select the input pin for the potentiometer
 double sensorValue = -1; // variable to store the value coming from the sensor
+uint32_t timer = millis();
 
 //***** AIR TEMP SENSOR *****
 #define AIRTEMP_BUS 4
 OneWire oneWireAir(AIRTEMP_BUS);
 DeviceAddress airThermometer;
+DallasTemperature sensorsAir(&oneWireAir);
 
 //***** WATER TEMP SENSOR *****
 #define WATERTEMP_BUS 5
 OneWire oneWireWater(WATERTEMP_BUS);
 DeviceAddress waterThermometer;
+DallasTemperature sensorsWater(&oneWireWater);
+
 
 //***** HUMID SENSOR *****
 #define dataPin A2
 #define sckPin A3 //serial clock
 SHT1x th_sensor(dataPin, sckPin);
 
-DallasTemperature sensorsAir(&oneWireAir);
-DallasTemperature sensorsWater(&oneWireWater);
+//***** GPS *****
+SoftwareSerial mySerial(3, 2);
+Adafruit_GPS GPS(&mySerial);
+#define GPSECHO  true
 
 void setup() {
+  //***** INITIALIZE MOSFET *****
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   digitalWrite(8, HIGH);
   digitalWrite(9, HIGH);  
   
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+
+  //**** GPS INTIALIZATION *****
+  GPS.begin(9600);
+  delay(1000);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PGCMD_ANTENNA);
+  delay(1000);
+  mySerial.println(PMTK_Q_RELEASE);
+  
   Serial.println("STARTING UP...");
   sensorsAir.begin();
   sensorsWater.begin();
@@ -43,11 +63,11 @@ void setup() {
   //***** INITIALIZE AIR TEMP SENSOR *****
   sensorsAir.setResolution(airThermometer, 9);
   sensorsWater.setResolution(waterThermometer, 9);
- 
 }
 
-void loop() { 
-  
+void loop() 
+{ 
+   
   //***** READ PHOTOCELL
   Serial.print("photocell: ");
   printLight();
@@ -63,7 +83,31 @@ void loop() {
   printHumidity();
   Serial.println();
 
-  delay(3000);
+  //***** READ GPS *****
+  printGPS();
+  delay(10000);
+}
+
+void printGPS() {
+  boolean fixAcq = false;
+
+  while(!fixAcq) {
+    char c = GPS.read();
+    GPS.parse(GPS.lastNMEA());
+    if(GPS.fix) {
+      Serial.print("Time: ");
+      Serial.print(GPS.hour, DEC); Serial.print(":");  Serial.print(GPS.minute, DEC); Serial.print(":");  Serial.println(GPS.seconds, DEC);
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", ");
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+      Serial.println();
+      fixAcq = true;
+    }  
+  }
+  
 }
 
 void printLight()
